384440ad7419bef194e41ae7412557ffb0c9ecaf
Papus <papus71email@gmail.com>
2020-04-24 02:05:09 +0200

    WEB_python fase II

--- index.py
+++ index.py
@@ -5,9 +5,14 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     return render_template('home.html')
-@app.route('/about')
-def about():
-    return render_template('about.html')
+
+@app.route('/cuba')
+def cuba():
+    return render_template('cuba.html')
+
+@app.route('/italia')
+def italia():
+    return render_template('italia.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
